e44851754433dad630aecf321f4489e60841949b
PatilTJ159 <58466429+PatilTJ159@users.noreply.github.com>
2025-06-12 08:52:43 +0530

    Update myfirsttestnotebook.py

--- myfirsttestnotebook.py
+++ myfirsttestnotebook.py
@@ -1,9 +1,12 @@
 from pyspark.sql import SparkSession
 
+
 def some_function(x):
     return x * 2
-  
+
+
 spark = SparkSession.builder.getOrCreate()
+
 # Databricks notebook source
 data = spark.range(1, 10).toDF("number")
 data.filter("number % 2 == 0").show()
