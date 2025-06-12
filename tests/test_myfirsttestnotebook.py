690f03998e203472b410e015a691890b042c6e8c
PatilTJ159 <58466429+PatilTJ159@users.noreply.github.com>
2025-06-12 23:02:40 +0530

    Update test_myfirsttestnotebook.py

--- tests/test_myfirsttestnotebook.py
+++ tests/test_myfirsttestnotebook.py
@@ -2,7 +2,7 @@ from myfirsttestnotebook import some_function
 
 
 def test_some_function():
-    #assert some_function(2) == 4
+    # assert some_function(2) == 4
     assert some_function(3) == 6
     assert some_function(0) == 0
     assert some_function(-1) == -2
