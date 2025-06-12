60aa46d0a6f56ec6067134cc1477464750c1d65f
PatilTJ159 <58466429+PatilTJ159@users.noreply.github.com>
2025-06-12 23:08:41 +0530

    Update myfirsttestnotebook.py

--- myfirsttestnotebook.py
+++ myfirsttestnotebook.py
@@ -4,14 +4,16 @@ from pyspark.sql import SparkSession
 def some_function(x):
     return x * 2
 
+
 def get_spark_session():
     return SparkSession.builder.getOrCreate()
 
-# Databricks notebook source
+
 def run_spark_job():
     spark = get_spark_session()
     data = spark.range(1, 10).toDF("number")
     data.filter("number % 2 == 0").show()
+
 
 if __name__ == "__main__":
     run_spark_job()
